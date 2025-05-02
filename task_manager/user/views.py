2b854b95447608fe0aea2b1412d86b70f1e0d71c
i-pichurov <deadbassein@gmail.com>
2025-05-02 14:00:28 +0300

    fix creation_form

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 
@@ -20,6 +22,12 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
+    def clean_password2(self):
+        password = self.cleaned_data.get('password2')
+        if len(password) < 3:
+            raise ValidationError(_('Пароль слишком короткий. Он должен содержать не менее 3 символов.'))
+        return password
 
 class UserCreateView(CreateView):
         form_class = CustomUserCreationForm
